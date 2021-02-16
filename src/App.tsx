c819992953d34d46674eff57f36a950d734d84a1
Hyemi Song <hyemisong@MacBook-Pro-3.local>
2021-02-15 19:10:29 -0800

    todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import CounterContainer from './containers/CounterContainer';
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <CounterContainer />;
+  return (
+    <>
+    <TodoInsert />
+    <TodoList />
+    </>
+  ) 
 }
 
 export default App;
